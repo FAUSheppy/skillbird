979282cae451a3d40a29f9cd7f633b0ff05d712f
Yannik Schmidt <sheppy@atlantishq.de>
2019-06-04 19:43:16 +0200

    basic http-api

---
+++ httpAPI.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import StorrageBackend as SB
+import flask
+
+
+app = flask.Flask("skillbird")
+
+################## HTML HELPER ########################
+def _invalidParameters():
+	return "500 - Invalid"
+
+########################################################
+
+@app.route('/getplayer')
+def getPlayer():
+	pname = flask.request.args.get("name")
+
+@app.route('/rankrange')
+def getRankRange('/rankrange'):
+	try:
+		start = int(flask.request.args.get("start"))
+		end   = int(flask.request.args.get("end"))
+		if end - start <= 0 or end - start > 100:
+			raise ValueError()
+	except ValueError:
+		return invalidParameters()
+
+	players = SB.getRankRange(start, end)
+	return "|".join([p.serialize() for p in players])
+
+@app.route('/findplayer')
+def getRankRange('/findplayer'):
+	string = flask.request.args.get("string")
+	players = SB.fuzzy_find_player(string)
+	return "|".join([p.serialize() for p in players])
