2f866db7236b456acbb5f742cdc21c9d44e1dda7
Yannik Schmidt <sheppy@atlantishq.de>
2019-06-08 16:06:40 +0200

    fix tab->spaces

--- httpAPI.py
+++ httpAPI.py
@@ -7,7 +7,7 @@ app = flask.Flask("skillbird")
 
 ################## HTML HELPER ########################
 def _invalidParameters():
-	return "500 - Invalid"
+    return "500 - Invalid"
 
 ########################################################
 
@@ -18,19 +18,20 @@ def getPlayer():
 
 @app.route('/rankrange')
 def getRankRange():
-	try:
-		start = int(flask.request.args.get("start"))
-		end   = int(flask.request.args.get("end"))
-		if end - start <= 0 or end - start > 100:
-			raise ValueError()
-	except ValueError:
-		return invalidParameters()
+    try:
+        start = int(flask.request.args.get("start"))
+        end   = int(flask.request.args.get("end"))
+        if end - start <= 0 or end - start > 100:
+            raise ValueError()
+    except ValueError:
+        return invalidParameters()
 
-	players = SB.getRankRange(start, end)
-	return "|".join([p.serialize() for p in players])
+    players = SB.getRankRange(start, end)
+    print(start, end)
+    return "|".join([p.serialize() for p in players])
 
 @app.route('/findplayer')
 def findPlayer():
-	string = flask.request.args.get("string")
-	players = SB.fuzzy_find_player(string)
-	return "|".join([p.serialize() for p in players])
+    string = flask.request.args.get("string")
+    players = SB.fuzzy_find_player(string)
+    return "|".join([p.serialize() for p in players])
